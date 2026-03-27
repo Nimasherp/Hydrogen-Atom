b062f218b5adc37428ba460994a7c91ac7baffec
Nimasherp <nimaparree@gmail.com>
2026-03-27 12:46:41 +0100

    2D cross section visualisation + images

--- simulation.py
+++ simulation.py
@@ -9,22 +9,22 @@ a0 = physical_constants["Bohr radius"][0]
 
 
 def spherical_harmonicY(theta, phi, l, m):
-    P = scipy.special.lpmv(m, l, np.cos(theta))
+    P = scipy.special.lpmv(abs(m), l, np.cos(theta))
     N = np.sqrt(
-        ((2*l + 1)*math.factorial(l - m)) / 
-        (4*math.pi*math.factorial(l + m))
+        ((2*l + 1)*math.factorial(l - abs(m))) / 
+        (4*math.pi*math.factorial(l + abs(m)))
     )
-    return (P * N * (np.exp(1j*m*phi)) * (-1)**m)
+    return (P * N * (np.cos(m*phi)) * (-1)**m)
 
 def radial_solutionR(r, n, l):
     k = n - l - 1
     alpha = 2*l + 1
     La = scipy.special.genlaguerre(k, alpha)
 
-    N = ((2*r)/(n*a0))
+    N = ((2*r)/(n*a0)) 
 
     # constante de normalisation assuring that the integration of the radial part around space is equal to 1
-    Norm = np.sqrt((2/(n*a0))**3 * math.factorial(k)/(2*n*math.factorial(n+l)))
+    Norm = np.sqrt((2/(n * a0))**3 * math.factorial(k)/(2 * n*math.factorial(n+l)))
 
 
     return Norm * (N**l) * np.exp(-N/2) * La(N)
@@ -40,7 +40,6 @@ class HydrogenOrbital:
         self.n = n
         self.l = l
         self.m = m
-        self.nb_of_pos = 100
 
     def psi(self, r, theta, phi):
         return (
@@ -57,13 +56,6 @@ class HydrogenOrbital:
 steps = 100
 # We set up space as 100 x 100 x 100 = 1 000 000 points where the max radius is defined.
 
-r = np.linspace(0, 20*a0, steps)
-theta = np.linspace(0, np.pi, steps)
-phi = np.linspace(0, 2*np.pi, steps)
-
-# Here merge will help us going from (k,k,k) to (i,j,k) positions.
-R, T, P = np.meshgrid(r, theta, phi, indexing="ij")
-
 n = int(input("Enter quantum number n : "))
 l = int(input(f"Enter azimuthal quantum number l (0<=l<n={n}): "))
 m = int(input(f"Enter magnetic quantum number m (-l <= m <= l, l={l}): "))
@@ -75,6 +67,14 @@ if not (-l <= m <= l):
     raise ValueError(f"m must be -l<= m <= l, got m={m}, l={l}")
 
 hydrogen = HydrogenOrbital(n,l,m)
+
+limit = n*10*a0
+r = np.linspace(0, limit, steps)
+theta = np.linspace(0, np.pi, steps)
+phi = np.linspace(0, 2*np.pi, steps)
+
+# Here merge will help us going from (k,k,k) to (i,j,k) positions.
+R, T, P = np.meshgrid(r, theta, phi, indexing="ij")
 
 # --------------- Monte Carlo simulation --------------- #
 # We first sample spatial points uniformly.
@@ -135,4 +135,35 @@ sc = ax.scatter(x, y, z, c=colors, cmap='jet', s=5, alpha=0.6)
 
 # Set camera 
 ax.view_init(elev=0, azim=0)
+
+ax.set_title(f'3D — orbital ({n},{l},{m})')
+
+# --------------- 2D visualisation --------------- #
+# This 2D visualisation is a cross section on the xz plan, so in this case y = 0 => phi = 0
+# In order to visualise the scatter we make a grid with x*z position.
+
+fig1, ax1 = plt.subplots()
+grid = 2000
+x_scatter = np.linspace(-limit, limit, grid)
+z_scatter = np.linspace(-limit, limit, grid)
+X2D, Z2D = np.meshgrid(x_scatter, z_scatter)
+
+r = np.sqrt(X2D**2 + Z2D**2)
+# We make sure to not divide by r = 0
+division = np.divide(Z2D, r, out=np.zeros_like(r), where=(r != 0))
+# Since arccos is defined on [-1;1] we don't want the division to be out of those bounds
+theta = np.arccos(np.clip(division, -1, 1))
+phi = np.full_like(r, 0)
+
+density = hydrogen.probability_density(r, theta, phi) * r**2 *4*np.pi
+
+img = ax1.imshow(density, cmap='magma')
+ax1.set_title(f'Cross-section 2D — orbital ({n},{l},{m})')
+
+plt.tight_layout()
+
 plt.show()
+
+# fig1.savefig(f"images/orbital ({n},{l},{m})")
+# --------------------------------------------------------------------------------------------------#
+
